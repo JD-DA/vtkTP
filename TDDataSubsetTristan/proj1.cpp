3dabcdf10a3eb01b158546101d32a9b0b5153660
Jean-Daniel de Ambrogi <jddea@icloud.com>
2023-03-13 14:30:05 +0100

    init tp tristan

--- TDDataSubsetTristan/proj1.cpp
+++ TDDataSubsetTristan/proj1.cpp
@@ -24,14 +24,17 @@ using std::endl;
 // Function prototypes
 vtkRectilinearGrid  *ReadGrid(int zStart, int zEnd);
 
+bool intersectPlane(const double *pDouble, const double *pDouble1, double d);
+
 bool PositiveSideOfPlane(const double *origin, const double *normal,
                          const double *point)
 {
     double sum = normal[0]*(origin[0]-point[0])
-               + normal[1]*(origin[1]-point[1])
-               + normal[2]*(origin[2]-point[2]);
+                 + normal[1]*(origin[1]-point[1])
+                 + normal[2]*(origin[2]-point[2]);
     return (sum < 0.);
 }
+
 
 void
 GetRelevantZRange(const double *origin, const double *normal, int *range)
@@ -39,21 +42,71 @@ GetRelevantZRange(const double *origin, const double *normal, int *range)
     // The data goes from 0 to 1 in X, Y, and Z.
     // There are gridSize points in each direction.
     // The i^{th} z-plane is located at i/(gridSize-1.0).
-    range[0] = 0;
-    //range[1] = gridSize-1;
-    range[1] = gridSize-1;
+    // range[0] = 0;
+    // range[1] = gridSize*0.5-1;
+    // range[1] = gridSize-1;
 
+    range[0] = -1;
+    range[1] = -1;
 
+    for(int i=0;i<gridSize;i++)
+    {
+        if ( intersectPlane(origin, normal,(double)i) && (range[0]==-1))
+        {
+            range[0]=i;
+
+        }
+
+        if( !intersectPlane(origin, normal,(double)i) && (range[1]==-1) && (range[0]!=-1)){
+            range[1] = i;
+        }
+
+        //if (range[0]==-1) range[0]=0;
+
+        //if (range[1]==-1) range[1]=-1;
+
+    }
 }
+
+bool intersectPlane(const double *origin, const double *normal,
+                    const double slice) {
+    double point[3];
+    point[0] = 0;
+    point[1] = 0;
+    point[2] = slice / (gridSize - 1.0);
+
+    bool side = PositiveSideOfPlane(origin, normal, point);
+
+
+    point[1] = 1;
+
+    if (side != PositiveSideOfPlane(origin, normal, point))
+        return true;
+
+    point[0] = 1;
+    point[1] = 0;
+
+    if (side != PositiveSideOfPlane(origin, normal, point))
+        return true;
+
+    point[0] = 1;
+    point[1] = 1;
+
+    if (side != PositiveSideOfPlane(origin, normal, point))
+        return true;
+
+    return false;
+}
+
 
 vtkRectilinearGrid *
 ReadRelevantGrid(const double *origin, const double *normal)
 {
     int range[2];
     GetRelevantZRange(origin, normal, range);
-  cerr << "Decided on " << range[0] << "-" << range[1] << endl;
+    cerr << "Decided on " << range[0] << "-" << range[1] << endl;
 
-    return ReadGrid(range[0], range[1]);
+    return ReadGrid(0, 511);
 }
 
 
@@ -62,62 +115,62 @@ int main(int argc, char *argv[])
     int t1;
     t1 = timer->StartTimer();
 
-   
-   double origin[3] = { 0.5, 0.5, 0.5 };
-   double normal[3] = { 0.1, 0.2, 0.974679 };
 
-   // Read the data.
+    double origin[3] = { 0.5, 0.5, 0.5 };
+    double normal[3] = { 0.1, 0.2, 0.974679 };
+
+    // Read the data.
     GetMemorySize("before read");
-   vtkRectilinearGrid *rg = ReadRelevantGrid(origin, normal);
-  GetMemorySize("after read");
-   vtkCutter *cutter = vtkCutter::New();
-   vtkPlane *plane = vtkPlane::New();
-   plane->SetNormal(normal);
-   plane->SetOrigin(origin);
-   cutter->SetCutFunction(plane);
-   cutter->SetInputData(rg);
+    vtkRectilinearGrid *rg = ReadRelevantGrid(origin, normal);
+    GetMemorySize("after read");
+    vtkCutter *cutter = vtkCutter::New();
+    vtkPlane *plane = vtkPlane::New();
+    plane->SetNormal(normal);
+    plane->SetOrigin(origin);
+    cutter->SetCutFunction(plane);
+    cutter->SetInputData(rg);
 
-   vtkDataSetMapper *mapper = vtkDataSetMapper::New();
-   mapper->SetInputConnection(cutter->GetOutputPort());
-   
-   vtkLookupTable *lut = vtkLookupTable::New();
-   mapper->SetLookupTable(lut);
-   mapper->SetScalarRange(0,15);
-   lut->Build();
+    vtkDataSetMapper *mapper = vtkDataSetMapper::New();
+    mapper->SetInputConnection(cutter->GetOutputPort());
 
-   vtkActor *actor = vtkActor::New();
-   actor->SetMapper(mapper);
+    vtkLookupTable *lut = vtkLookupTable::New();
+    mapper->SetLookupTable(lut);
+    mapper->SetScalarRange(0,15);
+
+    lut->Build();
+
+    vtkActor *actor = vtkActor::New();
+    actor->SetMapper(mapper);
 //actor->GetProperty()->SetRepresentationToWireframe();
-   vtkRenderer *ren = vtkRenderer::New();
-   ren->AddActor(actor);
+    vtkRenderer *ren = vtkRenderer::New();
+    ren->AddActor(actor);
 
-   vtkCamera *cam = ren->GetActiveCamera();
-   cam->SetFocalPoint(0.5, 0.5, 0.5);
-   cam->SetPosition(.5, 1.5, 1.5);
-
-   vtkRenderWindow *renwin = vtkRenderWindow::New();
-   renwin->SetSize(768, 768);
-   renwin->AddRenderer(ren);
+    vtkCamera *cam = ren->GetActiveCamera();
+    cam->SetFocalPoint(0.5, 0.5, 0.5);
+    cam->SetPosition(0.5, 0.5, 1.5);
 
 
-   vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+    vtkRenderWindow *renwin = vtkRenderWindow::New();
+    renwin->SetSize(768, 768);
+    renwin->AddRenderer(ren);
 
-   unsigned int n = 7;
-   double opacity = 1.0 / (static_cast<double>(n) * 5.0);
 
-   plane->SetNormal(ren->GetActiveCamera()->GetViewPlaneNormal());
+    vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+    unsigned int n = 7;
+    double opacity = 1.0 / (static_cast<double>(n)) * 5.0;
+
+    plane->SetNormal(ren->GetActiveCamera()->GetViewPlaneNormal());
     plane->SetOrigin(ren->GetActiveCamera()->GetFocalPoint());
+
 
     iren->SetRenderWindow(renwin);
     GetMemorySize("end");
     timer->StopTimer(t1,"time");
+    cutter->GenerateValues(n, -0.5, 0.5);
+    lut -> SetAlphaRange(opacity, opacity);
 
-   cutter->GenerateValues(n, -0.5, 0.5);
-   lut->SetAlphaRange(opacity,opacity);
-
-   renwin->Render();
-
-   iren->Start();
+    renwin->Render();
+    iren->Start();
 }
 
 
@@ -202,7 +255,7 @@ ReadGrid(int zStart, int zEnd)
  */
 
     rg->GetPointData()->SetActiveScalars("entropy");
-    
+
     cerr << FICHIER << "  Done reading" << endl;
     return rg;
 }
@@ -218,7 +271,7 @@ WriteImage(const char *name, const float *rgba, int width, int height)
 #else
     img->AllocateScalars(VTK_UNSIGNED_CHAR,3);
 #endif
-    
+
     for (int i = 0 ; i < width ; i++)
         for (int j = 0 ; j < height ; j++)
         {
@@ -228,16 +281,16 @@ WriteImage(const char *name, const float *rgba, int width, int height)
             ptr[1] = (unsigned char) (255*rgba[4*idx + 1]);
             ptr[2] = (unsigned char) (255*rgba[4*idx + 2]);
         }
-    
-    
+
+
     vtkPNGWriter *writer = vtkPNGWriter::New();
     writer->SetInputData(img);
     writer->SetFileName(name);
     writer->Write();
-    
+
     img->Delete();
     writer->Delete();
- 
+
 }
 
 
