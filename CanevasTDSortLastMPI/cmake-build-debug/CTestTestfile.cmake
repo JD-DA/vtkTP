10d45ef4167c3d5a2f6966f45472899e3db9e59d
Jean-Daniel de Ambrogi <jddea@icloud.com>
2023-03-20 15:00:24 +0100

    init tp 7

---
+++ CanevasTDSortLastMPI/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/o2183171@campus.univ-orleans.fr/bigdata/CanevasTDSortLastMPI
+# Build directory: /home/o2183171@campus.univ-orleans.fr/bigdata/CanevasTDSortLastMPI/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(SimpleTest "vtktp")
+set_tests_properties(SimpleTest PROPERTIES  _BACKTRACE_TRIPLES "/home/o2183171@campus.univ-orleans.fr/bigdata/CanevasTDSortLastMPI/CMakeLists.txt;98;add_test;/home/o2183171@campus.univ-orleans.fr/bigdata/CanevasTDSortLastMPI/CMakeLists.txt;0;")
